acd6ee062558883c76ac4fdb169b3b3d57b16c01
Mou <dhmourelle@gmail.com>
2020-12-01 21:54:34 +0100

    Solve Day 01: Report Repairt (Part 2)

--- day_01_report_repair/lib/day01_report_repair.ex
+++ day_01_report_repair/lib/day01_report_repair.ex
@@ -2,14 +2,23 @@ defmodule Day01ReportRepair do
   @doc """
 
   Examples:
-    iex> Day01ReportRepair.get_mult_with_sum([1721, 979, 366, 299, 675, 1456], 2020)
-    [{299, 1721, 2020, 514579}]
+    iex> Day01ReportRepair.get_mult_with_sum([1721, 979, 366, 299, 675, 1456], 2020, 2)
+    [{2020, 514579}]
+
+    iex> Day01ReportRepair.get_mult_with_sum([1721, 979, 366, 299, 675, 1456], 2020, 3)
+    [{2020, 241861950}]
   """
-  @spec get_mult_with_sum([integer], integer) :: [{integer, integer, integer, integer}]
-  def get_mult_with_sum(list, sum) do
-    for(x <- list, y <- list, x != y, do: [x, y] |> Enum.sort() |> List.to_tuple())
-    |> Enum.uniq()
-    |> Enum.map(fn {x, y} -> {x, y, x + y, x * y} end)
-    |> Enum.filter(fn {_, _, result, _} -> result == sum end)
+  @spec get_mult_with_sum([integer], integer, integer) :: [{integer, integer, integer, integer}]
+  def get_mult_with_sum(list, sum, size) do
+    combinations(size, list)
+    |> Enum.map(fn numbers -> {Enum.sum(numbers), Enum.reduce(numbers, &(&1 * &2))} end)
+    |> Enum.filter(fn {numbers_sum, _} -> numbers_sum == sum end)
+  end
+
+  defp combinations(0, _), do: [[]]
+  defp combinations(_, []), do: []
+
+  defp combinations(size, [h | t]) do
+    for(item <- combinations(size - 1, t), do: [h | item]) ++ combinations(size, t)
   end
 end
