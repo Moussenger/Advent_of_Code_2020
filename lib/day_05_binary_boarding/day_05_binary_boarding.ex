41b2cc8d2f1cc8be6a7c62d5e415f218e3b670cb
Mou <dhmourelle@gmail.com>
2020-12-05 11:05:37 +0100

    Solve Day 05: Binary Boarding Part 2

--- lib/day_05_binary_boarding/day_05_binary_boarding.ex
+++ lib/day_05_binary_boarding/day_05_binary_boarding.ex
@@ -1,13 +1,32 @@
 defmodule AdventOfCode.Day05BinaryBoarding do
   @doc """
   ## Examples
-    iex(10)> ["BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"] |> AdventOfCode.Day05BinaryBoarding.resolve_highest_seat()
+    iex> ["BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"] |> AdventOfCode.Day05BinaryBoarding.resolve_highest_seat()
     820
   """
   def resolve_highest_seat(seat_codes) do
     seat_codes
-    |> Stream.map(&resolve_seat_id/1)
+    |> resolve_seats_ids()
     |> Enum.max()
+  end
+
+  @doc """
+  ## Examples
+    iex> import AdventOfCode.Day05BinaryBoarding
+    iex> ["FFFFFFFLLR", "FFFFFFFLRL", "FFFFFFFRLL"] |> get_free_seat()
+    [{3, false}]
+  """
+  def get_free_seat(seat_codes) do
+    seat_ids = resolve_seats_ids(seat_codes)
+
+    Enum.min(seat_ids)..Enum.max(seat_ids)
+    |> Stream.map(&{&1, &1 in seat_ids})
+    |> Enum.filter(fn {_, presence} -> !presence end)
+  end
+
+  defp resolve_seats_ids(seat_codes) do
+    seat_codes
+    |> Enum.map(&resolve_seat_id/1)
   end
 
   defp resolve_seat_id(<<row::binary-size(7), column::bitstring>>) do
