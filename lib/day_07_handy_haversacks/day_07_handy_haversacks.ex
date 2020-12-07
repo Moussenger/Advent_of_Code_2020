ec8599f9dda5f0f4eca472894a6671348ee230bc
Mou <dhmourelle@gmail.com>
2020-12-08 00:34:45 +0100

    Solve Day 07: Handy Haversacks Part 2

--- lib/day_07_handy_haversacks/day_07_handy_haversacks.ex
+++ lib/day_07_handy_haversacks/day_07_handy_haversacks.ex
@@ -1,6 +1,7 @@
 defmodule AdventOfCode.Day07HandyHaversacks do
   @input_regex ~r/^(\w+ \w+) bags contain (.*).$/
   @bags_regex ~r/,?\s?\d (\w+ \w+) bag[s]?/
+  @bags_with_capacity_regex ~r/,?\s?(\d) (\w+ \w+) bag[s]?/
 
   @doc ~S"""
   ## Examples
@@ -10,7 +11,7 @@ defmodule AdventOfCode.Day07HandyHaversacks do
     4
   """
   def get_bag_colors_containing_at_least_one(bags_rules, bag) do
-    bags_rules = parse_bags_rules(bags_rules)
+    bags_rules = parse_bags_rules(bags_rules, &extract_bag_rule/1)
 
     bags_rules
     |> Map.keys()
@@ -19,14 +20,29 @@ defmodule AdventOfCode.Day07HandyHaversacks do
     |> Enum.count()
   end
 
-  defp parse_bags_rules(bags_rules) do
+  @doc ~S"""
+  ## Examples
+    iex> import AdventOfCode.Day07HandyHaversacks
+    iex> data = "shiny gold bags contain 2 dark red bags.\ndark red bags contain 2 dark orange bags.\ndark orange bags contain 2 dark yellow bags.\ndark yellow bags contain 2 dark green bags.\ndark green bags contain 2 dark blue bags.\ndark blue bags contain 2 dark violet bags.\ndark violet bags contain no other bags."
+    iex> data |> get_individual_bags_inside_of("shiny gold")
+    126
+  """
+
+  def get_individual_bags_inside_of(bags_rules, bag) do
+    bags_rules = parse_bags_rules(bags_rules, &extract_bag_with_capacity_rule/1)
+
+    bags_rules[bag]
+    |> count_bags(bags_rules)
+  end
+
+  defp parse_bags_rules(bags_rules, policy) do
     bags_rules
     |> String.split("\n")
-    |> Stream.map(&extract_bag_rule/1)
+    |> Stream.map(policy)
     |> Enum.reduce(%{}, fn {bag, bags}, map -> Map.put(map, bag, bags) end)
   end
 
-  def extract_bag_rule(bag_rule) do
+  defp extract_bag_rule(bag_rule) do
     [_ | [bag | [bags]]] = @input_regex |> Regex.run(bag_rule)
 
     bags =
@@ -35,6 +51,19 @@ defmodule AdventOfCode.Day07HandyHaversacks do
       |> Stream.map(&Regex.run(@bags_regex, &1))
       |> Stream.filter(&(&1 != nil))
       |> Enum.flat_map(fn [_ | match] -> match end)
+
+    {bag, bags}
+  end
+
+  defp extract_bag_with_capacity_rule(bag_rule) do
+    [_ | [bag | [bags]]] = @input_regex |> Regex.run(bag_rule)
+
+    bags =
+      bags
+      |> String.split(", ")
+      |> Stream.map(&Regex.run(@bags_with_capacity_regex, &1))
+      |> Stream.filter(&(&1 != nil))
+      |> Enum.map(fn [_ | [number | [name]]] -> {String.to_integer(number), name} end)
 
     {bag, bags}
   end
@@ -52,5 +81,13 @@ defmodule AdventOfCode.Day07HandyHaversacks do
       _ ->
         true
     end
+  end
+
+  defp count_bags([], _bags_rules), do: 0
+
+  defp count_bags(bags, bags_rules) do
+    bags
+    |> Enum.map(fn {quantity, bag} -> quantity + quantity * count_bags(bags_rules[bag], bags_rules) end)
+    |> Enum.sum()
   end
 end
