0f75f4194338730169fd2597bc1c9dd05e9f3380
Mou <dhmourelle@gmail.com>
2020-12-08 16:56:54 +0100

    Solve Day 08: Handle Halting Part 2

--- lib/day_08_handheld_halting/day_08_handle_halting.ex
+++ lib/day_08_handheld_halting/day_08_handle_halting.ex
@@ -3,13 +3,33 @@ defmodule AdventOfCode.Day08HandleHalting do
   ## Examples
     iex> import AdventOfCode.Day08HandleHalting
     iex> data = "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6"
-    iex> data |> run_program
-    5
-  """
-  def run_program(instructions) do
-    instructions = instructions |> parse_instructions()
+    iex> data |> run_program(&run_without_fix/1)
+    {:loop, 5}
 
-    run(instructions, Enum.at(instructions, 0), 0, [], false, 0)
+    iex> import AdventOfCode.Day08HandleHalting
+    iex> data = "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6"
+    iex> data |> run_program(&run_with_fix/1)
+    {:no_loop, 8}
+  """
+  def run_program(instructions, policy) do
+    instructions
+    |> parse_instructions()
+    |> policy.()
+  end
+
+  def run_without_fix(instructions), do: run(instructions, Enum.at(instructions, 0), 0, [], false, 0)
+
+  def run_with_fix(instructions), do: run_with_fix(instructions, instructions, 0)
+
+  defp run_with_fix(original_instructions, modified_instructions, last_try) do
+    case run(modified_instructions, Enum.at(modified_instructions, 0), 0, [], false, 0) do
+      {:loop, _} ->
+        {new_instructions, last_try} = fix_instructions(original_instructions, last_try)
+        run_with_fix(original_instructions, new_instructions, last_try)
+
+      result ->
+        result
+    end
   end
 
   defp parse_instructions(instructions) do
@@ -20,7 +40,26 @@ defmodule AdventOfCode.Day08HandleHalting do
     |> Enum.map(fn {instruction, arg} -> {instruction, String.to_integer(arg)} end)
   end
 
-  defp run(_instructions, _next, _next_pos, _previous_pos, true, accumulator), do: accumulator
+  defp fix_instructions(instructions, last_try) do
+    {previous_instructions, next_instructions} = Enum.split(instructions, last_try)
+
+    case do_fix_instructions(previous_instructions, next_instructions) do
+      :no_fix -> fix_instructions(instructions, last_try + 1)
+      new_instructions -> {new_instructions, last_try + 1}
+    end
+  end
+
+  defp do_fix_instructions(previous_instructions, [{"nop", arg} | last_instructions]) do
+    previous_instructions ++ [{"jmp", arg} | last_instructions]
+  end
+
+  defp do_fix_instructions(previous_instructions, [{"jmp", arg} | last_instructions]) do
+    previous_instructions ++ [{"nop", arg} | last_instructions]
+  end
+
+  defp do_fix_instructions(_previous_instructions, _last_instructions), do: :no_fix
+
+  defp run(_instructions, _next, _next_pos, _previous_pos, true, accumulator), do: {:loop, accumulator}
 
   defp run(instructions, {"acc", arg}, next_pos, previous_pos, _repeated, accumulator) do
     next_pos = next_pos + 1
@@ -43,7 +82,7 @@ defmodule AdventOfCode.Day08HandleHalting do
     run(instructions, Enum.at(instructions, next_pos), next_pos, [next_pos | previous_pos], repeated, accumulator)
   end
 
-  defp run(_instructions, _instruction, _next_pos, _previous_pos, _repeated, _accumulator) do
-    System.halt(-1)
+  defp run(_instructions, _instruction, _next_pos, _previous_pos, _repeated, accumulator) do
+    {:no_loop, accumulator}
   end
 end
