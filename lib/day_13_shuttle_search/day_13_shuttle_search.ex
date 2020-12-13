a5336c5069cba8457d160e2f9ae2725111908946
Mou <dhmourelle@gmail.com>
2020-12-13 13:53:18 +0100

    Solve Day 13: Shuttle Search Par 2

--- lib/day_13_shuttle_search/day_13_shuttle_search.ex
+++ lib/day_13_shuttle_search/day_13_shuttle_search.ex
@@ -1,4 +1,6 @@
 defmodule AdventOfCode.Day13ShuttleSearch do
+  import AdventOfCode.Helpers.ChineseRemainder
+
   @unknown_ids "x"
   @doc ~S"""
   ## Examples
@@ -11,6 +13,19 @@ defmodule AdventOfCode.Day13ShuttleSearch do
     schedule
     |> parse_schedule()
     |> resolve_next_departure()
+  end
+
+  @doc ~S"""
+  ## Examples
+    iex> import AdventOfCode.Day13ShuttleSearch
+    iex> data = "939 7,13,x,x,59,x,31,19"
+    iex> data |> get_bus_timestamp_for_correlation()
+    1068781
+  """
+  def get_bus_timestamp_for_correlation(schedule) do
+    schedule
+    |> parse_schedule_for_correlation()
+    |> chinese_remainder()
   end
 
   defp parse_schedule(schedule) do
@@ -30,6 +45,19 @@ defmodule AdventOfCode.Day13ShuttleSearch do
     {current_timestamp, frequencies}
   end
 
+  defp parse_schedule_for_correlation(schedule) do
+    {_current_timestamp, frequencies} =
+      schedule
+      |> String.split()
+      |> List.to_tuple()
+
+    frequencies
+    |> String.split(",")
+    |> Stream.with_index()
+    |> Enum.filter(fn {frequency, _} -> frequency != @unknown_ids end)
+    |> Enum.map(fn {frequency, index} -> {String.to_integer(frequency), -index} end)
+  end
+
   defp resolve_next_departure({current_timestamp, frequencies}) do
     remaining_frequencies = calc_remaining_frequencies(frequencies, current_timestamp)
     resolve_next_departure(current_timestamp, frequencies, remaining_frequencies, current_timestamp)
@@ -39,6 +67,8 @@ defmodule AdventOfCode.Day13ShuttleSearch do
     departure_position = remaining_frequencies |> Enum.find_index(&(&1 == 0))
     next_timestamp = current_timestamp + 1
     remaining_frequencies = calc_remaining_frequencies(frequencies, next_timestamp)
+
+    # IO.inspect({current_timestamp, timestamp, departure_position})
 
     case departure_position do
       nil -> resolve_next_departure(timestamp, frequencies, remaining_frequencies, next_timestamp)
---
+++ lib/helpers/chinese_remainder.ex
@@ -0,0 +1,52 @@
+defmodule AdventOfCode.Helpers.ChineseRemainder do
+  @moduledoc """
+  Thans to code-shoily for this translation of chinese remainder calculator to Elixir
+  """
+  def chinese_remainder(congruences) do
+    {modulii, residues} = Enum.unzip(congruences)
+    mod_pi = List.foldl(modulii, 1, &Kernel.*/2)
+    crt_modulii = Enum.map(modulii, &div(mod_pi, &1))
+
+    case calc_inverses(crt_modulii, modulii) do
+      nil ->
+        nil
+
+      inverses ->
+        crt_modulii
+        |> Enum.zip(
+          residues
+          |> Enum.zip(inverses)
+          |> Enum.map(fn {a, b} -> a * b end)
+        )
+        |> Enum.map(fn {a, b} -> a * b end)
+        |> Enum.sum()
+        |> mod(mod_pi)
+    end
+  end
+
+  def egcd(_, 0), do: {1, 0}
+
+  def egcd(a, b) do
+    {s, t} = egcd(b, rem(a, b))
+    {t, s - div(a, b) * t}
+  end
+
+  defp mod_inv(a, b) do
+    {x, y} = egcd(a, b)
+    (a * x + b * y == 1 && x) || nil
+  end
+
+  defp mod(a, m) do
+    x = rem(a, m)
+    (x < 0 && x + m) || x
+  end
+
+  defp calc_inverses([], []), do: []
+
+  defp calc_inverses([n | ns], [m | ms]) do
+    case mod_inv(n, m) do
+      nil -> nil
+      inv -> [inv | calc_inverses(ns, ms)]
+    end
+  end
+end
