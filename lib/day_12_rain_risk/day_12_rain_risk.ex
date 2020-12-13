a253c1d5b91da96b8d0da32764944596d882c24f
Mou <dhmourelle@gmail.com>
2020-12-13 10:25:53 +0100

    Solve Day 12: Rain Risk Part 2

--- lib/day_12_rain_risk/day_12_rain_risk.ex
+++ lib/day_12_rain_risk/day_12_rain_risk.ex
@@ -30,6 +30,19 @@ defmodule AdventOfCode.Day12RainRisk do
     |> run_instructions(@east, {0, 0})
   end
 
+  @doc ~S"""
+  ## Examples
+    iex> import AdventOfCode.Day12RainRisk
+    iex> data = "F10 N3 F7 R90 F11"
+    iex> data |> resolve_manhattan_distance_with_waypoint({10, 1})
+    286
+  """
+  def resolve_manhattan_distance_with_waypoint(instructions, waypoint) do
+    instructions
+    |> parse_instructions()
+    |> run_instructions_with_wp(waypoint, {0, 0})
+  end
+
   defp parse_instructions(instructions) do
     instructions
     |> String.split()
@@ -58,6 +71,24 @@ defmodule AdventOfCode.Day12RainRisk do
     run_instructions(instructions, turn_direction(turn, direction, value), {x, y})
   end
 
+  defp run_instructions_with_wp([], _waypoint, {x, y}), do: abs(x) + abs(y)
+
+  defp run_instructions_with_wp([{@forward, value} | instructions], {wx, wy}, {x, y}) do
+    run_instructions_with_wp(instructions, {wx, wy}, {x + wx * value, y + wy * value})
+  end
+
+  defp run_instructions_with_wp([{direction, value} | instructions], {wx, wy}, point) when direction in @vertical do
+    run_instructions_with_wp(instructions, {wx, wy + value_for_direction(value, direction)}, point)
+  end
+
+  defp run_instructions_with_wp([{direction, value} | instructions], {wx, wy}, point) when direction in @horizontal do
+    run_instructions_with_wp(instructions, {wx + value_for_direction(value, direction), wy}, point)
+  end
+
+  defp run_instructions_with_wp([{turn, value} | instructions], waypoint, point) when turn in @turn do
+    run_instructions_with_wp(instructions, turn_waypoint(turn, waypoint, value), point)
+  end
+
   defp value_for_direction(value, direction) when direction in @negative, do: -value
   defp value_for_direction(value, direction) when direction in @positive, do: +value
 
@@ -70,4 +101,8 @@ defmodule AdventOfCode.Day12RainRisk do
     move = div(rem(degrees, 360), 90)
     Enum.at(@points, rem((@points |> Enum.find_index(&(&1 == direction))) - move, length(@points)))
   end
+
+  defp turn_waypoint(_direction, waypoint, 0), do: waypoint
+  defp turn_waypoint(@right, {wx, wy}, degrees), do: turn_waypoint(@right, {wy, -wx}, degrees - 90)
+  defp turn_waypoint(@left, {wx, wy}, degrees), do: turn_waypoint(@left, {-wy, wx}, degrees - 90)
 end
