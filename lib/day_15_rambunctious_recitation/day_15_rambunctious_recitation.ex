238841e31217cf122c247e84ab342fa403aea8c1
Mou <dhmourelle@gmail.com>
2020-12-15 18:46:16 +0100

    Solve Day 15: Rambunctious recitation Part 2

--- lib/day_15_rambunctious_recitation/day_15_rambunctious_recitation.ex
+++ lib/day_15_rambunctious_recitation/day_15_rambunctious_recitation.ex
@@ -9,26 +9,30 @@ defmodule AdventOfCode.Day15RambunctiousRecitation do
     [436, 1, 10, 27, 78, 438, 1836]
   """
   def get_nth_spoken(initial, nth) do
-    initial
-    |> String.split(@separator)
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.with_index(1)
-    |> Enum.reverse()
-    |> resolve_nth(nth)
+    numbers =
+      initial
+      |> String.split(@separator)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.with_index(1)
+
+    last_value = List.last(numbers)
+    olds_map = Enum.into(numbers, %{})
+    resolve_nth(last_value, olds_map, nth - Enum.count(numbers))
   end
 
-  defp resolve_nth([{last_value, _} | _values] = numbers, nth) when length(numbers) == nth, do: last_value
+  defp resolve_nth({last_value, _last_index}, _olds_map, 0), do: last_value
 
-  defp resolve_nth([{last_value, last_index} | values] = numbers, nth) do
-    repeated = Enum.find(values, fn {value, _} -> value == last_value end)
+  defp resolve_nth({last_value, last_index}, olds_map, nth) do
+    repeated = Map.get(olds_map, last_value)
+    olds_map = Map.put(olds_map, last_value, last_index)
     new_index = last_index + 1
 
     new_value =
       case repeated do
         nil -> {0, new_index}
-        {_, old_index} -> {last_index - old_index, new_index}
+        old_index -> {last_index - old_index, new_index}
       end
 
-    resolve_nth([new_value | numbers], nth)
+    resolve_nth(new_value, olds_map, nth - 1)
   end
 end
