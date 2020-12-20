a4e181187b60f445c79199ef33abaff25ff92a3f
Mou <dhmourelle@gmail.com>
2020-12-20 16:18:46 +0100

    Solve Day 18: Operation Order Part 2

--- lib/day_18_operation_order/README.md
+++ lib/day_18_operation_order/README.md
@@ -31,3 +31,26 @@ Here are a few more examples:
 5 _ 9 _ (7 _ 3 _ 3 + 9 _ 3 + (8 + 6 _ 4)) becomes 12240.
 ((2 + 4 _ 9) _ (6 + 9 _ 8 + 6) + 6) + 2 + 4 \* 2 becomes 13632.
 Before you can help with the homework, you need to understand it yourself. Evaluate the expression on each line of the homework; what is the sum of the resulting values?
+
+## --- Part Two ---
+
+You manage to answer the child's questions and they finish part 1 of their homework, but get stuck when they reach the next section: advanced math.
+
+Now, addition and multiplication have different precedence levels, but they're not the ones you're familiar with. Instead, addition is evaluated before multiplication.
+
+For example, the steps to evaluate the expression 1 + 2 _ 3 + 4 _ 5 + 6 are now as follows:
+
+1 + 2 _ 3 + 4 _ 5 + 6
+3 _ 3 + 4 _ 5 + 6
+3 _ 7 _ 5 + 6
+3 _ 7 _ 11
+21 \* 11
+231
+Here are the other examples from above:
+
+1 + (2 _ 3) + (4 _ (5 + 6)) still becomes 51.
+2 _ 3 + (4 _ 5) becomes 46.
+5 + (8 _ 3 + 9 + 3 _ 4 _ 3) becomes 1445.
+5 _ 9 _ (7 _ 3 _ 3 + 9 _ 3 + (8 + 6 _ 4)) becomes 669060.
+((2 + 4 _ 9) _ (6 + 9 _ 8 + 6) + 6) + 2 + 4 \* 2 becomes 23340.
+What do you get if you add up the results of evaluating the homework problems using these new rules?
--- lib/day_18_operation_order/day_18_operation_order.ex
+++ lib/day_18_operation_order/day_18_operation_order.ex
@@ -4,11 +4,8 @@ defmodule AdventOfCode.Day18OperationOrder do
 
   @old_sum "+"
   @new_sum "<~"
+  @new_sum_precedence "^^^"
 
-  # import AdventOfCode.Day18OperationOrder
-  # data = "2 * 3 + (4 * 5)\n5 + (8 * 3 + 9 + 3 * 4 * 3)\n5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))\n((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
-  # data |> sum_all_operations()
-  # file = "lib/day_18_operation_order/input.txt" |> File.read!()
   @doc ~S"""
   ## Examples
     iex> import AdventOfCode.Day18OperationOrder
@@ -18,20 +15,39 @@ defmodule AdventOfCode.Day18OperationOrder do
   """
   def sum_all_operations(operations) do
     operations
-    |> parse_operations()
+    |> parse_operations(@new_mult, @new_sum)
+    |> run_operations()
+  end
+
+  @doc ~S"""
+  ## Examples
+    iex> import AdventOfCode.Day18OperationOrder
+    iex> data = "2 * 3 + (4 * 5)\n5 + (8 * 3 + 9 + 3 * 4 * 3)\n5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))\n((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
+    iex> data |> sum_all_operations_with_other_precedence()
+    693891
+  """
+  def sum_all_operations_with_other_precedence(operations) do
+    operations
+    |> parse_operations(@new_mult, @new_sum_precedence)
+    |> run_operations()
+  end
+
+  defp parse_operations(operations, new_mult, new_sum) do
+    operations
+    |> String.replace(@old_mult, new_mult)
+    |> String.replace(@old_sum, new_sum)
+    |> String.split("\n")
+  end
+
+  defp run_operations(operations) do
+    operations
     |> Stream.map(&Code.eval_string("import AdventOfCode.Day18OperationOrder;" <> &1))
     |> Stream.map(fn {res, _} -> res end)
     |> Stream.filter(&is_integer/1)
     |> Enum.sum()
   end
 
-  defp parse_operations(operations) do
-    operations
-    |> String.replace(@old_mult, @new_mult)
-    |> String.replace(@old_sum, @new_sum)
-    |> String.split("\n")
-  end
-
   def a <~ b, do: a + b
+  def a ^^^ b, do: a + b
   def a ~> b, do: a * b
 end
